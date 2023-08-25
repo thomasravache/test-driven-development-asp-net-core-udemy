ee210970a8ba8aa4edef45095a9e56f6b3e00db0
Thomas Ravache <thomasravache31@gmail.com>
2023-08-24 23:17:58 -0300

    feat: 04. Create Test Project

--- projects/RoomBookingApp/RoomBookingApp.Core.Tests/UnitTest1.cs
+++ projects/RoomBookingApp/RoomBookingApp.Core.Tests/UnitTest1.cs
@@ -5,6 +5,10 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // Arrange -> preparar o teste -> criar objetos e instanciar dados necessários
 
+        // Act -> simular uma chamada a uma ação
+
+        // Assert -> Verificação se o teste irá passar ou não, como o comportamento precisa ser
     }
 }
